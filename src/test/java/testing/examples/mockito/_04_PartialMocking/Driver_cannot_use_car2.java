42805782511c4622b82e487055688c7f43fc9314
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-01-29 14:40:00 +0200

    organized imports

--- src/test/java/testing/examples/mockito/_04_PartialMocking/Driver_cannot_use_car2.java
+++ src/test/java/testing/examples/mockito/_04_PartialMocking/Driver_cannot_use_car2.java
@@ -3,9 +3,7 @@ package testing.examples.mockito._04_PartialMocking;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
